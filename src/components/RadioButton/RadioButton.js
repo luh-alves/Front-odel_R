85ed879c0d02c6434c93b54ae51c751813f963be
Luciana Alves <luh.ana.af@gmail.com>
2019-09-06 15:11:46 -0300

    Fix Radio  Layout

--- src/components/RadioButton/RadioButton.js
+++ src/components/RadioButton/RadioButton.js
@@ -1,21 +1,30 @@
-import React, {Component} from 'react';
-import {Radio} from "react-radio-group";
+import React, { Component } from 'react';
+import { Radio } from "react-radio-group";
 import './RadioButton.css';
 
 
 class RadioButton extends Component {
+    isCurrentSelected(){
+        return this.props.selected === this.props.engine.kwh;
+    }
     render() {
         return (
             <div className="RadioLayout">
-                <label className="RadioContainer">
-                    <span className="KilowattsModel">{this.props.engine.kwh}</span>
-                    <span className="ModelType">{this.props.engine.type}</span>
-                    <span className="Kilowatts">{this.props.engine.kwh} kwh</span>
+                <label className={this.isCurrentSelected()? 'Black RadioContainer':'Grey RadioContainer' } >
+                    <span className="KilowattsModel">
+                        {this.props.engine.kwh}
+                        <span className={this.isCurrentSelected()? ' Red ModelType':'Grey ModelType'}> {this.props.engine.type}</span>
+                    </span>
+
+                    <span >{this.props.engine.kwh} kwh</span>
+
                     <span className="MilesRange">{this.props.engine.range} miles range</span>
-                    <Radio value={this.props.engine.kwh} id={this.props.engine.id} checked={this.props.engine.id===1}/>
-                    <span className="Checkmark"/>
+
+                    <Radio value={this.props.engine.kwh} id={this.props.engine.id} checked={this.props.engine.id === 1} />
+                    <span className="Checkmark" />
+
                 </label>
-                {this.props.engine.price > 0 && this.props.selected === this.props.engine.kwh ?
+                {this.props.engine.price > 0 && this.isCurrentSelected() ?
                     <span className="AdditionalPrice">+ {this.props.engine.price}</span> : null}
             </div>
         );
