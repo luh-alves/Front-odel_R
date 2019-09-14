50d7cddf4e62b4fab6f345b2a49890a27039e64e
Luciana Alves <luh.ana.af@gmail.com>
2019-09-14 15:02:46 -0300

    Fix button begin

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react';
 import './HomeScreen.css';
 import begin from '../../images/red-arrow.png';
 import redR from '../../images/red-R.png';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 class HomeScreen extends Component {
     render() {
         return (
-            <div className="Home">
-                {/* <Link to="/enginescreen">Ir para a página Engine \o/</Link>         */}
-
+            <div className='Home'>
                 <div className='HomeTextContainer'>
                     <h2>Build your</h2>
                     <h1>MODEL <span className='redR'>R</span> </h1>
                 </div>
                 <div className='ModelRContainer'>
-                    <img className='redR' src={redR}/>
-                    <Link to="/engine"><h6>BEGIN <img className='begin' src={begin}/></h6></Link>
+                    <img className='redR' src={redR} />
+                    <div  className='BeginText'>
+                        <button className='StyleButton' onClick={this.props.goToNextScreen}>
+                            <span>B E G I N  <img className='BeginImage' src={begin} /> </span>
+                        </button>
+                    </div>
                 </div>
             </div>
         );
